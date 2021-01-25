e39ffe8ed91da4c616a5b22701e64c479c69b708
dipen211 <pateldipen161@gmail.com>
2021-01-25 18:36:41 +0530

    commit

--- src/comopnents/Register/register.test.tsx
+++ src/comopnents/Register/register.test.tsx
@@ -8,57 +8,48 @@ describe('<Register>', function () {
 
     it('Should capture firstname correctly onChange', () => {
         const wrapper = shallow(<Register />);
-        const input = wrapper.find('input').at(0);
+        wrapper.find('input').at(0);
         wrapper.simulate('change');
-        wrapper.instance().firstname = 'Dipen';
+        wrapper.setState({firstname: "Dipen"})
         expect(wrapper.state('firstname')).toEqual('Dipen');
     })
 
-    // it('Should capture lastname correctly onChange', () => {
+    it('Should capture lastname correctly onChange', () => {
+        const wrapper = shallow(<Register />);
+        wrapper.find('input').at(1);
+        wrapper.simulate('change');
+        wrapper.setState({lastname: "Dipen"})
+        expect(wrapper.state('lastname')).toEqual('Dipen');
+    })
 
-    //     const wrapper = mount(<Register />);
-    //     const input = wrapper.find('input').at(1);
-    //     input.instance().value = 'Dipen';
-    //     input.simulate('change');
-    //     expect(wrapper.state().lastname).toEqual('Dipen');
-    // })
+    it('Should capture email correctly onChange and change the props accordingly', () => {
+        const wrapper = shallow(<Register />);
+        wrapper.find('input').at(2);
+        wrapper.setState({email: "Dipen@gmail,com"})
+        wrapper.simulate('change');
+        expect(wrapper.find('input').at(2).props().value).toEqual('Dipen@gmail.com');
+    })
 
-    // it('Should capture email correctly onChange and change the props accordingly', () => {
+    it('Should capture email correctly onChange and change the state accordingly', () => {
+        const wrapper = shallow(<Register />);
+        wrapper.find('input').at(2);
+        wrapper.simulate('change');
+        wrapper.setState({email: "Dipen@gmail,com"})
+        expect(wrapper.state('email')).toEqual('Dipen@gmail,com');
+    })
+    
+    it('Should call alert() when submit button is clicked', () => {
+        const state = {
+            firstname: 'Dipen', lastname: 'Dipen',
+            email: 'Dipen@gmail.com',
+            subscribed: false
+        };
+        const expectedArg = "Firstname: Dipen, Lastname: Dipen, Email: Dipen@gmail.com, Subscribed: No";
+        const wrapper = shallow(<Register />);
+        window.alert = jest.fn();
+        wrapper.setState(state)
 
-    //     const wrapper = mount(<Register />);
-    //     const input = wrapper.find('input').at(2);
-    //     input.instance().value = 'Dipen@gmail.com';
-    //     input.simulate('change');
-    //     expect(wrapper.find('input').at(2).props().value).toEqual('Dipen@gmail.com');
-    // })
-
-    // it('Should capture email correctly onChange and change the state accordingly', () => {
-
-    //     const wrapper = mount(<Register />);
-    //     const input = wrapper.find('input').at(2);
-    //     input.instance().value = 'mail@hotmail.com';
-    //     input.simulate('change');
-    //     expect(wrapper.state().email).toEqual('mail@hotmail.com');
-    // })
-    // it('Should capture checkbox ticked correctly onChange', () => {
-    //     const wrapper = mount(<Register />);
-    //     const input = wrapper.find('input').at(3);
-    //     input.instance().checked = true;
-    //     input.simulate('change');
-    //     expect(wrapper.state().subscribed).toEqual(true);
-    // })
-    // it('Should call alert() when submit button is clicked', () => {
-    //     const state = {
-    //         firstname: 'hello', lastname: 'world',
-    //         email: 'hello@world.com', languages: ['English', 'French'],
-    //         subscribed: true
-    //     };
-    //     const expectedArg = "Firstname: hello, Lastname: world, Email: hello@world.com, Language: English,French, Subscribed: Yes";
-    //     const wrapper = mount(<Register />);
-    //     window.alert = jest.fn();
-    //     wrapper.setState(state)
-
-    //     wrapper.find('form').simulate('submit')
-    //     expect(window.alert).toHaveBeenCalledWith(expectedArg);
-    // })
+        wrapper.find('form').simulate('submit')
+        expect(window.alert).toHaveBeenCalledWith(expectedArg);
+    })
 })
